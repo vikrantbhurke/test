5bcc73c7eefa33221a4d940fab1d28a92dd5c8b9
Vikrant Bhurke <bhurkevi@gmail.com>
2025-06-11 16:05:21 +0530

    create sendEmail dev and prod modules and conditionally import them

---
+++ src/features/user/modules/send-email-dev.ts
@@ -0,0 +1,27 @@
+import { transport } from "@/global/configurations/nodemailer";
+import { TemplateVariables } from "mailtrap";
+
+export const sendEmailDev = async (
+  filename: string,
+  recipient: string,
+  subject: string,
+  variables: TemplateVariables
+) => {
+  const { template } = await import(`@/global/templates/${filename}`);
+  const Handlebars = (await import("handlebars")).default;
+  const handlerbarsTemplate = Handlebars.compile(template);
+
+  const mailOptions = {
+    from: process.env.MAILTRAP_SENDER as string,
+    to: recipient,
+    subject,
+    html: handlerbarsTemplate(variables),
+  };
+
+  try {
+    await transport.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
---
+++ src/features/user/modules/send-email-prod.ts
@@ -0,0 +1,32 @@
+import { MailtrapClient, TemplateVariables } from "mailtrap";
+
+export const sendEmailProd = async (
+  id: string,
+  recipient: string,
+  variables: TemplateVariables
+) => {
+  const client = new MailtrapClient({
+    token: process.env.MAILTRAP_TOKEN as string,
+  });
+
+  const sender = {
+    email: process.env.MAILTRAP_SENDER as string,
+    name: process.env.APP_NAME as string,
+  };
+
+  const recipients = [{ email: recipient }];
+
+  const mailOptions = {
+    from: sender,
+    to: recipients,
+    template_uuid: id,
+    template_variables: variables,
+  };
+
+  try {
+    await client.send(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
